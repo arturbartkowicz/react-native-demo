ea4625033e4917c12dc2e93fad6c33973c1f0ddf
Artur Bartkowicz <artur.bartkowicz1@gmail.com>
2021-06-15 16:32:34 +0200

    Added some Buttons for testing

--- App.js
+++ App.js
@@ -1,12 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Button, Pressable } from 'react-native';
 
 export default function App() {
+  const [isBtnActive, setIsBtnActive] = useState(true);
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.header}>{ isBtnActive ? 'Well-being button' : 'Btn was clicked' }</Text>
       <StatusBar style="auto" />
+      <Pressable 
+        style={styles.button} 
+        onPress={() => {
+          setIsBtnActive(!isBtnActive);
+        }}>
+        <Text style={styles.text}>{isBtnActive ? "Active" : "Disabled"}</Text>
+      </Pressable>
     </View>
   );
 }
@@ -14,8 +22,27 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#FFCD00',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  header: {
+    backgroundColor: '#F27B08',
+  },
+  button: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingVertical: 12,
+    paddingHorizontal: 32,
+    borderRadius: 4,
+    elevation: 3,
+    backgroundColor: 'black',
+  },
+  text: {
+    fontSize: 16,
+    lineHeight: 21,
+    fontWeight: 'bold',
+    letterSpacing: 0.25,
+    color: 'white',
   },
 });
